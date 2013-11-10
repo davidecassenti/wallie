e516a908adce265f3d316f954360011777d48020
bettytran <bettyhtran@gmail.com>
2013-11-10 13:11:22 +0000

    map

--- client/app/controllers/coupon.js
+++ client/app/controllers/coupon.js
@@ -4,5 +4,6 @@ $.couponTitle.text = args.es_info.text;
 $.companyLogo.image = args.pic.image;
 
 $.mapButton.addEventListener('click', function(e){
-	
+	var map = Alloy.createController('map', {}).getView();
+	Alloy.Globals.nav.openWindow(map);
 });
---
+++ client/app/controllers/map.js
@@ -0,0 +1,8 @@
+function setRegion(){
+	$.mapview.region = {
+		latitude: "51.5787053",
+		longitude: "-0.1240043",
+		latitudeDelta: 0.1,
+		longitudeDelta: 0.1
+	};
+}
